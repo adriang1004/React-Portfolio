866f1ef820fc07aed915fc127c3ec2790296c09c
adrian garcia <adriang1004@gmail.com>
2024-02-19 15:38:55 -0600

    finished routing to working pages and links

--- react-portfolio/src/main.jsx
+++ react-portfolio/src/main.jsx
@@ -1,10 +1,42 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 import App from './App.jsx'
 import './index.css'
+import Contact from './pages/Contact.jsx';
+import About from './pages/About.jsx';
+import Portfolio from './pages/Portfolio.jsx';
+import Resume from './pages/Resume.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+    children: [
+      {
+        index: true,
+        element: <About />,
+      },
+      {
+        path: '/About',
+        element: <About />,
+      },
+      {
+        path: '/Portfolio',
+        element: <Portfolio />,
+      },
+      {
+        path: '/Contact',
+        element: <Contact />,
+      },
+      {
+        path: '/Resume',
+        element: <Resume />,
+      },
+    ],
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+  <RouterProvider router={router} />
+);
---
+++ react-portfolio/src/pages/Resume.jsx
@@ -0,0 +1,15 @@
+function Resume() {
+    return (
+      <section>
+        <h2>Resume</h2>
+        <a href="path_to_resume">Download My Resume</a>
+        <ul>
+          <li>Proficiency 1</li>
+          <li>Proficiency 2</li>
+        </ul>
+      </section>
+    );
+  }
+  
+  export default Resume;
+  
